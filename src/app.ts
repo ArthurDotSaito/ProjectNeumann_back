ffa4b4c466a57d807425cb9f5a550d186a5411d1
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-02 20:56:43 -0300

    chore: config database

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,21 @@ import 'express-async-errors';
 import express, { Express } from 'express';
 import cors from 'cors';
 
+import { loadEnv, connectDb, disconnectDB } from '@/config';
+
+loadEnv();
+
 const app = express();
 
 app.use(cors()).use(express.json());
+
+export function init(): Promise<Express> {
+	connectDb();
+	return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+	await disconnectDB();
+}
+
+export default app;
