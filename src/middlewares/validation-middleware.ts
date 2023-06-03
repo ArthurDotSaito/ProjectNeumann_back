578f1eb8380a13f21798ee1ee26d763515e99eb0
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-03 15:56:51 -0300

    chore: adding erros and middlewares directory

---
+++ src/middlewares/validation-middleware.ts
@@ -0,0 +1,23 @@
+import { ObjectSchema } from 'joi';
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+
+export function validateBody<T>(schema: ObjectSchema<T>): ValidationMiddleware {
+	return validate(schema, 'body');
+}
+
+function validate(schema: ObjectSchema, type: 'body' | 'params') {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const { error } = schema.validate(req[type], {
+			abortEarly: false,
+		});
+
+		if (!error) {
+			next();
+		} else {
+			res.status(httpStatus.BAD_REQUEST).send(invalidDataError(error.details.map((d) => d.message)));
+		}
+	};
+}
+
+type ValidationMiddleware = (req: Request, res: Response, next: NextFunction) => void;
