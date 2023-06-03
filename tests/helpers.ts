bfcec6365e1eae7193636d1e3f05eb41475ff591
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-03 18:08:04 -0300

    test: adding users-factories

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,5 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import { User } from '@prisma/client';
+import { createUser } from './factories';
+import { createSession } from './factories/sessions-factory';
 import { prisma } from '@/config';
 
 export async function cleanDb() {
