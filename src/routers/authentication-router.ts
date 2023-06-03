6d89ee8965528cf26a87654dcfeb5865f9d4e2dd
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-03 17:43:50 -0300

    feat: finishing signIn route

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { validateBody } from '@/middlewares';
 import { signInSchema } from '@/schemas/authentication-schema';
+import { singInPost } from '@/controller';
 
 const authenticationRouter = Router();
-authenticationRouter.post('/sign-in', validateBody(signInSchema));
+authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
 
 export { authenticationRouter };
