b3ebad0716888b485f2bb73a38636a91c958c0e6
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-16 14:35:31 -0300

    feat: adding details on submit controller

--- src/controller/submitCode-controller.ts
+++ src/controller/submitCode-controller.ts
@@ -1,7 +1,16 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 
 import { AuthenticatedRequest } from '@/middlewares/authentication-middleware';
+import { SubmitCodeData } from '@/protocols';
+import submitServices from '@/services/submit-services';
 
-export default function submitCode(req: AuthenticatedRequest, res: Response): any {
-	return null;
+export async function submitCode(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+	const codeSubmission = req.body as SubmitCodeData;
+	try {
+		const compileResponse = await submitServices.compileCode(codeSubmission);
+		const token = compileResponse.token;
+		await submitServices.checkSubmitTokenStatus(token);
+	} catch (error) {
+		next(error);
+	}
 }
