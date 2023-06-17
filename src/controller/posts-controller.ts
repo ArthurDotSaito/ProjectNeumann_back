c15e14141f33d1529501497acbee0ab56347e325
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-17 19:29:31 -0300

    feat: finishing post-router

--- src/controller/posts-controller.ts
+++ src/controller/posts-controller.ts
@@ -6,6 +6,10 @@ import postsServices from '@/services/posts-services';
 export async function getAllPosts(req: AuthenticatedRequest, res: Response, next: NextFunction) {
 	try {
 		await postsServices.createPostFromData();
+
+		const postFromDatabase = await postsServices.getAllPosts();
+
+		return res.status(500).send(postFromDatabase);
 	} catch (error) {
 		next(error);
 	}
--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares/authentication-middleware';
+import { getAllPosts } from '@/controller/posts-controller';
 
 const postsRouter = Router();
 
 postsRouter.all('/*', authenticateToken);
-postsRouter.get('/');
+postsRouter.get('/', getAllPosts);
 
 export { postsRouter };
