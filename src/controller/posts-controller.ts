e3e0d09274374a4bfcfc94a2eb0a7ad1f5e7c946
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-18 14:05:40 -0300

    feat: adding route and controller for postById

--- src/controller/posts-controller.ts
+++ src/controller/posts-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Response } from 'express';
 
+import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares/authentication-middleware';
 import postsServices from '@/services/posts-services';
 
@@ -9,6 +10,17 @@ export async function getAllPosts(req: AuthenticatedRequest, res: Response, next
 		const postFromDatabase = await postsServices.getAllPosts();
 
 		return res.status(201).send(postFromDatabase);
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function getPostById(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+	const { postId } = req.params;
+	try {
+		const post = await postsServices.getPostById(parseInt(postId));
+
+		return res.status(httpStatus.OK).send(post);
 	} catch (error) {
 		next(error);
 	}
--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares/authentication-middleware';
-import { getAllPosts } from '@/controller/posts-controller';
+import { getAllPosts, getPostById } from '@/controller/posts-controller';
 
 const postsRouter = Router();
 
 postsRouter.all('/*', authenticateToken);
 postsRouter.get('/', getAllPosts);
+postsRouter.get('/:postId', getPostById);
 
 export { postsRouter };
