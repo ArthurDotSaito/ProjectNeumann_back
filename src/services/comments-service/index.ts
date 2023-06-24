1f6487af317ce4e19b1f168524369c0dc3ca2c2b
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-24 16:10:50 -0300

    feat: add postComment function on comment service

--- src/services/comments-service/index.ts
+++ src/services/comments-service/index.ts
@@ -1,16 +1,27 @@
+import { forBiddenError } from '@/errors/forbidden-error';
 import { notFoundError } from '@/errors/notFound-error';
 import commentsRepository from '@/repositories/comments-repository';
 
 async function getAllCommentsFromPost(postId: number) {
-	if (!postId) throw notFoundError();
+	if (!postId) throw forBiddenError();
 
 	const comments = await commentsRepository.getAllCommentsFromPost(postId);
 
 	return comments;
 }
 
+async function postComment(content: string, postId: number, userId: number) {
+	if (!postId) throw forBiddenError();
+	if (!userId) throw forBiddenError();
+
+	const comment = await commentsRepository.postComment(content, postId, userId);
+
+	return comment;
+}
+
 const commentsServices = {
 	getAllCommentsFromPost,
+	postComment,
 };
 
 export default commentsServices;
