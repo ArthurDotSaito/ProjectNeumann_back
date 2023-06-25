bd3a2470fba36f58e02f3273b1d7ad87803395f6
Arthur Saito <arthur_saito1998@hotmail.com>
2023-06-25 12:11:37 -0300

    feat: implemeting first post.test

---
+++ tests/integration/posts.test.ts
@@ -0,0 +1,46 @@
+import supertest from 'supertest';
+import httpStatus from 'http-status';
+import { faker } from '@faker-js/faker';
+import * as jwt from 'jsonwebtoken';
+
+import { cleanDb, generateValidToken } from '../helpers';
+import { createUser } from '../factories';
+import app, { init } from '@/app';
+
+beforeAll(async () => {
+	await init();
+});
+
+beforeEach(async () => {
+	await cleanDb();
+});
+
+const server = supertest(app);
+
+describe('GET All /posts', () => {
+	describe('When token is invalid or there is no session', () => {
+		it('should respond with status 401 if no token is given', async () => {
+			const response = await server.get('/posts');
+
+			expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+		});
+
+		it('should respond with status 401 if given token is not valid', async () => {
+			const token = faker.lorem.word();
+
+			const response = await server.get('/posts').set('Authorization', `Bearer ${token}`);
+
+			expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+		});
+
+		it('should respond with status 401 if there is no session for given token', async () => {
+			const userWithoutSession = await createUser();
+			const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+			const response = await server.get('/posts').set('Authorization', `Bearer ${token}`);
+
+			expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+		});
+	});
+	describe('When token is valid', () => {});
+});
